25a87c4cb874c43ce604ee6a95bfafd3bd889f49
IgnacioZDev7 <ignaciozarate.dev@gmail.com>
2026-02-25 21:09:04 -0400

    logica del cambio de nombre implementada

--- src/app/components/avatar/avatar.ts
+++ src/app/components/avatar/avatar.ts
@@ -14,6 +14,15 @@ export class AvatarComponent {
  tieneLentes = signal<boolean>(false);
  tipoExpresion = signal<number>(1);
 
+
+//se agregan nuevas señales para manejar nombre y giro
+//almacena el texto temporal escrito en el input
+ nombreInput = signal<string>('');
+//almacena el nombre confirmado que se muestra junto al avatar
+ nombreGuardado = signal<string>('');
+//booleano que controla si la animación de giro está activa
+ girando = signal<boolean>(false);
+
   cambiarColor(event: Event){
     const input = event.target as HTMLInputElement;
     this.colorFondo.set(input.value);
@@ -28,4 +37,18 @@ export class AvatarComponent {
  setExpresion(tipo: number){
     this.tipoExpresion.set(tipo);
  }
+
+ //se agregan nuevos metodos 
+ // actualiza el nombre 
+ cambiarNombre(event: Event){
+   const input = event.target as HTMLInputElement;
+   this.nombreInput.set(input.value);
+ }
+
+ // replica el valor actual para mostrarlo junto al avatar
+ guardarNombre(){
+   this.nombreGuardado.set(this.nombreInput());
+ }
+
+ 
 }
