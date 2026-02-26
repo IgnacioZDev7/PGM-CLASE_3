83206b53a4f2811864aa70df85a04a44f1956359
IgnacioZDev7 <ignaciozarate.dev@gmail.com>
2026-02-25 20:46:29 -0400

    segundo commit, logica del avatar en la clase implementada

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {AvatarComponent} from './components/avatar/avatar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, AvatarComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('PGM-CLASE_3');
+ title = "clase Gráfica"
 }
---
+++ src/app/components/avatar/avatar.ts
@@ -0,0 +1,31 @@
+import { Component, signal } from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+@Component({
+  selector: 'app-avatar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './avatar.html',
+  styleUrl: './avatar.scss',
+})
+export class AvatarComponent {
+ colorFondo = signal<string>('#3498db');
+ tamano = signal<number>(150);
+ tieneLentes = signal<boolean>(false);
+ tipoExpresion = signal<number>(1);
+
+  cambiarColor(event: Event){
+    const input = event.target as HTMLInputElement;
+    this.colorFondo.set(input.value);
+  }
+ cambiarTamano(event: Event){
+   const input = event.target as HTMLInputElement;
+   this.tamano.set(parseInt(input.value));
+ }
+ toggleLentes(){
+    this.tieneLentes.update(flag => !flag);
+ }
+ setExpresion(tipo: number){
+    this.tipoExpresion.set(tipo);
+ }
+}
